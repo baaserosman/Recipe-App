5a6ac6ea909e3673d843bbb7a685ba3e84738f5a
baaserosman <baaser.osman@gmail.com>
2022-03-27 15:44:38 +0200

    .

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,30 @@
-import AboutStyles from "./AboutStyles";
+import { PDiv, Img, Span, Link } from "./AboutStyles";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <Img
+        src="https://cdn.pixabay.com/photo/2015/01/08/18/26/man-593333_1280.jpg"
+        alt=""
+      />
+      <h2>
+        About Software Developer <Span>BASCHER</Span>
+      </h2>
+
+      <PDiv>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quae
+          nemo delectus deserunt repellat. Nemo excepturi, a ipsa beatae soluta
+          odio autem voluptatem sit natus! Ipsa cumque recusandae ratione fugit
+          excepturi fuga quisquam nobis veritatis, placeat officia. Velit eum
+          reprehenderit eveniet ea quae laborum officia repellendus quaerat
+          consectetur possimus. Quas!
+        </p>
+        <Span>Email : </Span>
+        <Link href="#">example@gmail.com </Link>
+      </PDiv>
+    </div>
+  );
 };
 
 export default About;
