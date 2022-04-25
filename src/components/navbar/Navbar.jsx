2babf3c8162d6c09e98f2622ba844f1eb10a6516
baaserosman <baaser.osman@gmail.com>
2022-04-26 00:43:20 +0200

    navbar modified

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <Nav justify="space-between" wrap="wrap">
       <Logo to="/">
-        <i>{"<Clarusway>"}</i>
+        <i>{"<Bascher>"}</i>
         <span>RECIPE</span>
       </Logo>
 
